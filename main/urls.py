31e2bb714e2593d9a2df5a8a32abc39c840af59b
iotarepeat <dhruvam15@gmail.com>
2019-10-03 20:24:42 +0530

    - Changed Front page - Resolved minor error in upload_success

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('upload_success', views.uploaded_link, name="upload_success"),
-    path('p/<public_link>', views.public_link_handle, name="public_link"),
+    path('<public_link>', views.public_link_handle, name="public_link"),
     path('analytics/<analytic_link>', views.analytic_link_handle, name="analytic_link"),
     path('csv/<analytic_link>', views.downloadAsCsv, name="download_csv"),
 ]
